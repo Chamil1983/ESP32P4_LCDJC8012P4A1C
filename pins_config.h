5db6498cdbedc51ff03fd9dbfd9ea14c860ded31
Microcode Embedded Systems and Automation <120463632+Chamil1983@users.noreply.github.com>
2025-06-15 14:47:15 +1000

    Update pins_config.h

--- pins_config.h
+++ pins_config.h
@@ -10,3 +10,11 @@
 #define TP_I2C_SCL 8
 #define TP_RST 22
 #define TP_INT 21
+
+// **CRITICAL: SD Card SDIO pins from SDMMC_Test.ino**
+#define SDMMC_CLK_PIN 43
+#define SDMMC_CMD_PIN 44
+#define SDMMC_D0_PIN 39
+#define SDMMC_D1_PIN 40
+#define SDMMC_D2_PIN 41
+#define SDMMC_D3_PIN 42
